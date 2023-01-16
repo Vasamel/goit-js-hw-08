5b720d9ea91bbef12471fc0323737484abf0de60
Vasyl Melnyk <109175872+Vasamel@users.noreply.github.com>
2023-01-16 19:36:23 +0200

    Add files via upload

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,4 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 
@@ -9,27 +9,27 @@ const LOCAL_DATA = 'feedback-form-state';
 let date = {};
 
 if (localStorage.getItem(LOCAL_DATA)) {
-    const parsedData = JSON.parse(localStorage.getItem(LOCAL_DATA));
-    form.elements.email.value = parsedData.email || '';
-    form.elements.message.value = parsedData.message || '';
+  const parsedData = JSON.parse(localStorage.getItem(LOCAL_DATA));
+  form.elements.email.value = parsedData.email || '';
+  form.elements.message.value = parsedData.message || '';
 }
 
 function onInput(e) {
-    date[e.target.name] = e.target.value;
+  let date = localStorage.getItem('feedback-form-state');
+  date = date ? JSON.parse(date) : {};
+  date[e.target.name] = e.target.value;
 
-    const stringifyDate = JSON.stringify(date);
-    localStorage.setItem(LOCAL_DATA, stringifyDate);
+  const stringifyDate = JSON.stringify(date);
+  localStorage.setItem(LOCAL_DATA, stringifyDate);
 }
 
 function onClickButton(e) {
-    const parsedData = JSON.parse(localStorage.getItem(LOCAL_DATA));
-    console.log(parsedData);
+  const parsedData = JSON.parse(localStorage.getItem(LOCAL_DATA));
+  console.log(parsedData);
 
-    e.preventDefault();
-    localStorage.removeItem(LOCAL_DATA);
-    e.currentTarget.reset();
+  e.preventDefault();
+  localStorage.removeItem(LOCAL_DATA);
+  e.currentTarget.reset();
 
-    let date = '';
-    
+  let date = '';
 }
-
